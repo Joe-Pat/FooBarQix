4f9808d00305519aac4524b9443f9a58fb4f15ac
Joachim Patora <joachim.patora@gmail.com>
2021-02-07 19:46:17 +0100

    Added tests linked with Step 2 rules

--- test/main/java/stringrules/StringParserTest.java
+++ test/main/java/stringrules/StringParserTest.java
@@ -19,17 +19,39 @@ public class StringParserTest {
 	@Test
 	@DisplayName("Result is empty")
 	public void testEncodeStringNotContainingExpectedCaracters() {
-		assertEquals("", stringParser.encodeString("0124689"));
+		assertEquals("", stringParser.encodeString("124689"));
 		assertNotEquals("", stringParser.encodeString("0123456789"));
+	}
+	
+	@Test
+	@DisplayName("Result is *")
+	public void testEncodingStringContaining0() {
+		assertEquals("*", stringParser.encodeString("0"));
+		assertNotEquals("*", stringParser.encodeString("0123456789"));
+	}
+	
+	@Test
+	@DisplayName("Result is *Foo or Foo*")
+	public void testEncodeStringContaining0And3() {
+		assertEquals("*Foo", stringParser.encodeString("03"));
+		assertEquals("Foo*", stringParser.encodeString("30"));
+	}
+	
+	@Test
+	@DisplayName("Result is *Foo or Foo*")
+	public void testOrderEncodeString() {
+		assertEquals("*FooQixBar", stringParser.encodeString("0375"));
+		assertEquals("Foo*Bar", stringParser.encodeString("305"));
+		assertEquals("BarFooQixQixBar**", stringParser.encodeString("53775001"));
 	}
 	
 	@Test
 	@DisplayName("Result is Foo")
 	public void testEncodeStringContaining3Not5Not7() {
 		assertEquals("Foo", stringParser.encodeString("3"));
-		assertEquals("Foo", stringParser.encodeString("01234689"));
-		assertEquals("Foo", stringParser.encodeString("302"));
-		assertEquals("Foo", stringParser.encodeString("3204"));
+		assertEquals("Foo", stringParser.encodeString("1234689"));
+		assertEquals("Foo", stringParser.encodeString("32"));
+		assertEquals("Foo", stringParser.encodeString("324"));
 		assertNotEquals("Foo", stringParser.encodeString("0123456789"));
 	}
 	
@@ -37,9 +59,9 @@ public class StringParserTest {
 	@DisplayName("Result is Bar")
 	public void testEncodeStringContaining5Not3Not7() {
 		assertEquals("Bar", stringParser.encodeString("5"));
-		assertEquals("Bar", stringParser.encodeString("01245689"));
-		assertEquals("Bar", stringParser.encodeString("502"));
-		assertEquals("Bar", stringParser.encodeString("5204"));
+		assertEquals("Bar", stringParser.encodeString("1245689"));
+		assertEquals("Bar", stringParser.encodeString("52"));
+		assertEquals("Bar", stringParser.encodeString("524"));
 		assertNotEquals("Bar", stringParser.encodeString("0123456789"));
 	}
 	
@@ -47,9 +69,9 @@ public class StringParserTest {
 	@DisplayName("Result is Qix")
 	public void testEncodeStringContaining7Not3Not5() {
 		assertEquals("Qix", stringParser.encodeString("7"));
-		assertEquals("Qix", stringParser.encodeString("01246789"));
-		assertEquals("Qix", stringParser.encodeString("702"));
-		assertEquals("Qix", stringParser.encodeString("7204"));
+		assertEquals("Qix", stringParser.encodeString("1246789"));
+		assertEquals("Qix", stringParser.encodeString("72"));
+		assertEquals("Qix", stringParser.encodeString("724"));
 		assertNotEquals("Qix", stringParser.encodeString("0123456789"));
 	}
 	
@@ -57,8 +79,8 @@ public class StringParserTest {
 	@DisplayName("Result is FooBar")
 	public void testEncodeStringContaining3And5Not7() {
 		assertEquals("FooBar", stringParser.encodeString("35"));
-		assertEquals("FooBar", stringParser.encodeString("012345689"));
-		assertEquals("FooBar", stringParser.encodeString("3050"));
+		assertEquals("FooBar", stringParser.encodeString("12345689"));
+		assertEquals("FooBar", stringParser.encodeString("35"));
 		assertEquals("FooBar", stringParser.encodeString("325"));
 		assertEquals("FooBar", stringParser.encodeString("352"));
 		assertEquals("FooBar", stringParser.encodeString("3254"));
@@ -69,9 +91,9 @@ public class StringParserTest {
 	@DisplayName("Result is BarQix")
 	public void testEncodeStringContaining5And7Not3() {
 		assertEquals("BarQix", stringParser.encodeString("57"));
-		assertEquals("BarQix", stringParser.encodeString("507"));
-		assertEquals("BarQix", stringParser.encodeString("012456789"));
-		assertEquals("BarQix", stringParser.encodeString("5072"));
+		assertEquals("BarQix", stringParser.encodeString("57"));
+		assertEquals("BarQix", stringParser.encodeString("12456789"));
+		assertEquals("BarQix", stringParser.encodeString("572"));
 		assertEquals("BarQix", stringParser.encodeString("5274"));
 		assertNotEquals("BarQix", stringParser.encodeString("0123456789"));
 	}
@@ -80,15 +102,16 @@ public class StringParserTest {
 	@DisplayName("Result is FooQix")
 	public void testEncodeStringContaining3And7Not5() {
 		assertEquals("FooQix", stringParser.encodeString("37"));
-		assertEquals("FooQix", stringParser.encodeString("012346789"));
-		assertEquals("FooQix", stringParser.encodeString("3702"));
-		assertEquals("FooQix", stringParser.encodeString("32704"));
+		assertEquals("FooQix", stringParser.encodeString("12346789"));
+		assertEquals("FooQix", stringParser.encodeString("372"));
+		assertEquals("FooQix", stringParser.encodeString("3274"));
 		assertNotEquals("FooQix", stringParser.encodeString("0123456789"));
 	}
 	
 	@Test
-	@DisplayName("Result is FooQix")
+	@DisplayName("Result is *FooBarQix")
 	public void testEncodeStringContaining3And5And7() {
-		assertEquals("FooBarQix", stringParser.encodeString("0123456789"));
+		assertEquals("*FooBarQix", stringParser.encodeString("0123456789"));
+		assertNotEquals("", stringParser.encodeString("0123456789"));
 	}
 }
